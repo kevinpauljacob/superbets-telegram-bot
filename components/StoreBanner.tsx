1ffc049109a68e0e969bc96b6b1739352fd0cea9
itaintkevin <kpjtofficial@gmail.com>
2024-03-08 22:04:43 +0530

    feat: Store page created

---
+++ components/StoreBanner.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Banner from "/public/assets/storeBanner.svg";
+
+export default function StoreBanner() {
+  return (
+    <div className="relative w-full my-7">
+      <Image
+        className="z-0 w-full"
+        src={Banner}
+        width="100"
+        height="100"
+        alt="Store Banner"
+      />
+      <div className="absolute top-0 left-0 z-10 px-16 py-8 w-[50%]">
+        <div className="rounded-full bg-[#9945FF] w-min px-10 py-1.5">
+          Store
+        </div>
+        <h2 className="text-5xl md:text-3xl sm:text-xl my-4">
+          A Store to <span className="text-[#9945FF]">FOMO</span> About
+        </h2>
+        <p className="text-xl md:text-lg text-gray-400">
+          Sake your FOMO tokens to receive multipliers for your points which
+          gets you amazing rewards from our store
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ components/StoreCard.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Card from "/public/assets/Card.png";
+
+export default function StoreCard({ src, name, points }) {
+  return (
+    <div className="bg-[#171717] rounded-md min-w-[260px] mb-5">
+      <div>
+        <Image
+          className="rounded-md w-full"
+          src={Card}
+          alt={name}
+          width={100}
+          height={100}
+        />
+      </div>
+      <div className="px-3">
+        <div className="text-center font-bold my-3">
+          <p className="text-xs text-[#9945FF]">{name}</p>
+          <p className="text-xl">{points} points</p>
+        </div>
+        <button className="text-xl font-bold bg-[#9945FF] rounded-md w-full py-2 mb-4">
+          Redeem
+        </button>
+      </div>
+    </div>
+  );
+}
