a5d12c4b4d2feefb32e81f0853855584a3545922
jxhnsebastian <john.babysebastian@gmail.com>
2024-03-19 07:47:42 +0530

    fix: store grid

--- components/StoreCard.tsx
+++ components/StoreCard.tsx
@@ -9,7 +9,7 @@ interface StoreCardProps {
 
 export default function StoreCard({ src, name, points }: StoreCardProps) {
   return (
-    <div className="bg-[#171717] rounded-md min-w-[240px] mb-5">
+    <div className="bg-[#171717] rounded-md w-full mb-5">
       <div>
         <Image
           className="rounded-md w-full"
@@ -22,9 +22,9 @@ export default function StoreCard({ src, name, points }: StoreCardProps) {
       <div className="px-3">
         <div className="text-center font-bold my-3">
           <p className="text-xs text-[#9945FF]">{name}</p>
-          <p className="text-xl">{points} points</p>
+          <p className="text-lg sm:text-xl">{points} points</p>
         </div>
-        <button className="text-xl font-bold bg-[#9945FF] rounded-md w-full py-2 mb-4">
+        <button className="text-base sm:text-xl font-bold bg-[#9945FF] rounded-md w-full py-1 sm:py-2 mb-4">
           Redeem
         </button>
       </div>
