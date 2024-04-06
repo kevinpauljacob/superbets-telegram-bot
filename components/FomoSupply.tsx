9fbd112ad1080026a735e94e52aae14fc3cb2cd8
itaintkevin <kpjtofficial@gmail.com>
2024-04-06 18:30:56 +0530

    fomosupply responsive changes

--- components/FomoSupply.tsx
+++ components/FomoSupply.tsx
@@ -17,7 +17,7 @@ export default function FomoSupply() {
   const { burnt, circulating } = FomoSupply[0];
 
   return (
-    <div className="hidden md:block lg:w-[250px] xl:w-[300px] 2xl:w-[330px]">
+    <div className="hidden md:block lg:w-[250px] xl:w-[280px] 2xl:w-[330px]">
       <div className="bg-[#1E2220] rounded-lg lg:p-3 xl:p-4 ">
         <div className="flex justify-between items-center w-full">
           <span className="font-semibold text-gray-400 text-sm xl:text-base">
@@ -25,7 +25,7 @@ export default function FomoSupply() {
           </span>
           <Image src={info} alt="" width={13} height={13} />
         </div>
-        <div className="bg-gradient-to-r from-blue-500 to-purple-700 rounded-xl lg:px-2 xl:px-2.5 py-2.5 2xl:py-4 my-2.5">
+        <div className="bg-gradient-to-r from-blue-500 to-purple-700 rounded-xl lg:px-2 xl:px-2.5 lg:py-2 xl:py-2.5 2xl:py-4 my-1.5 xl:my-2 2xl:my-2.5">
           <div className="flex justify-between items-center rounded-full bg-[#2E2C2F]/20 lg:p-3 xl:p-4">
             <Image src={fire} alt="" width={20} height={17} />
             <Image
@@ -46,7 +46,7 @@ export default function FomoSupply() {
               ></span>
             </div>
           </div>
-          <div className="rounded-xl bg-[#2E2C2F]/20 p-4 my-3">
+          <div className="rounded-xl bg-[#2E2C2F]/20 p-4 my-2 2xl:my-3">
             <div className="flex items-center text-xs 2xl:text-sm w-full">
               <span className="bg-[#EA5252] rounded-full h-[4px] lg:w-[20px] xl:w-[30px] 2xl:w-[40px]"></span>
               <span className="text-gray-300 font-semibold px-2">
@@ -58,7 +58,7 @@ export default function FomoSupply() {
               234,546,654
             </div>
           </div>
-          <div className="rounded-xl bg-[#2E2C2F]/20 p-4 my-2.5">
+          <div className="rounded-xl bg-[#2E2C2F]/20 p-4 my-1.5 2xl:my-2">
             <div className="flex items-center text-xs 2xl:text-sm w-full">
               <span className="bg-[#72F238] rounded-full h-[4px] lg:w-[20px] xl:w-[30px] 2xl:w-[40px]"></span>
               <span className="text-gray-300 font-semibold px-2">
@@ -82,8 +82,8 @@ export default function FomoSupply() {
           </span>
         </div>
       </div>
-      <div className="bg-[#1E2220]/75 rounded-lg lg:p-3 xl:p-4 mt-4">
-        <div className="bg-gradient-to-r from-[#D129FA] to-[#1EE0AF] rounded-lg py-3 lg:px-2 xl:px-4 mb-3">
+      <div className="bg-[#1E2220]/75 rounded-lg lg:p-3 xl:p-4 mt-2 xl:mt-3 2xl:mt-4">
+        <div className="bg-gradient-to-r from-[#D129FA] to-[#1EE0AF] rounded-lg py-2 xl:py-3 lg:px-2 xl:px-4 mb-2 2xl:mb-3">
           <div className="flex items-start justify-between">
             <div className="flex items-center">
               <p className="lg:text-xl xl:text-2xl 2xl:text-3xl font-bold font-changa">
@@ -106,7 +106,7 @@ export default function FomoSupply() {
           onClick={() => {
             router.push("stake");
           }}
-          className="bg-[#1E2220] hover:bg-[#262827] transition-all cursor-pointer text-[#883CE3] text-md 2xl:text-lg font-semibold text-center rounded-lg p-2.5"
+          className="bg-[#1E2220] hover:bg-[#262827] transition-all cursor-pointer text-[#883CE3] text-md 2xl:text-lg font-semibold text-center rounded-lg px-2.5 py-2 2xl:p-2.5"
         >
           Stake $FOMO
         </div>
