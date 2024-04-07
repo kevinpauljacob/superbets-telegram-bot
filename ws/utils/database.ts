2c6f091244791ec296b3deae430fd8f301533156
Alwin Helor <alwin24012002@gmail.com>
2024-04-07 20:45:03 +0530

    adding tsconfig

--- ws/utils/database.ts
+++ ws/utils/database.ts
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env.local" });
 declare global {
   var mongoose: any; // This must be a `var` and not a `let / const`
 }
@@ -7,7 +9,7 @@ const MONGODB_URI = process.env.DB_URI!;
 
 if (!MONGODB_URI) {
   throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local"
+    "Please define the MONGODB_URI environment variable inside .env.local",
   );
 }
 
