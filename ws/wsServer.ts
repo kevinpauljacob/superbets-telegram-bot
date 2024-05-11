3a37dd4534fa7914a024d52a03fba4e1e753a16c
eibelsalil <eibelsm@gmail.com>
2024-05-11 17:14:12 +0530

    handle ws error

--- ws/wsServer.ts
+++ ws/wsServer.ts
@@ -12,13 +12,13 @@ const port = parseInt(process.env.PORT || "4000");
 
 const connection = new Connection(process.env.BACKEND_RPC!, "confirmed");
 const devWalletKey = Keypair.fromSecretKey(
-  Uint8Array.from(JSON.parse(process.env.DEV_KEYPAIR!)),
+  Uint8Array.from(JSON.parse(process.env.DEV_KEYPAIR!))
 );
 const fomoProgramId = new PublicKey(
-  "FoMotN3mJB5QVorWrgF7gHRoguUYRr2dApDondesrYe",
+  "FoMotN3mJB5QVorWrgF7gHRoguUYRr2dApDondesrYe"
 );
 const fomoJupProgramId = new PublicKey(
-  "3ZVntpabyX5dh71gHeyEHcqUhV7Gu3irUT25tm8eQPnJ",
+  "3ZVntpabyX5dh71gHeyEHcqUhV7Gu3irUT25tm8eQPnJ"
 );
 
 const server = new WebSocket.Server({ port });
@@ -34,7 +34,7 @@ const channelClients: { [key: string]: Set<WebSocket> } = {};
 const handleFomoBuyEvent = async (
   programId: PublicKey,
   IDL: any,
-  dbName: string,
+  dbName: string
 ) => {
   const provider = new AnchorProvider(connection, new Wallet(devWalletKey), {
     commitment: "processed",
@@ -83,13 +83,14 @@ const handleListenerClient = (socket: WebSocket, message: any) => {
     if (!channelClients[channel]) {
       channelClients[channel] = new Set();
     }
+
     channelClients[channel].add(socket);
 
     socket.send(
       JSON.stringify({
         success: true,
         clientsCount: channelClients[channel].size,
-      }),
+      })
     );
   } else {
     socket.send(JSON.stringify({ success: false }));
@@ -101,16 +102,18 @@ try {
   handleFomoBuyEvent(fomoJupProgramId, fomoJupIDL, "jupexit");
 
   server.on("connection", (socket: WebSocket) => {
-    console.log("server connected")
+    console.log("server connected");
     socket.on("message", (data) => {
-      console.log("server msg received")
+      console.log("server msg received");
       try {
         const message = JSON.parse(data.toString());
 
-        if (message.clientType === "api-client") {
-          handleAPIClient(socket, message);
-        } else if (message.clientType === "listener-client") {
-          handleListenerClient(socket, message);
+        if (socket.readyState == WebSocket.OPEN) {
+          if (message.clientType === "api-client") {
+            handleAPIClient(socket, message);
+          } else if (message.clientType === "listener-client") {
+            handleListenerClient(socket, message);
+          }
         }
 
         // Handle other types of clients or messages here if needed
@@ -119,10 +122,15 @@ try {
       }
     });
 
-    socket.on('error', console.error);
+    socket.on("error", (e: any) => {
+      console.log("socket error: ", e);
+      Object.values(channelClients).forEach((clients) => {
+        clients.delete(socket);
+      });
+    });
 
     socket.on("close", () => {
-      console.log("server closed")
+      console.log("server closed");
       // Remove the disconnected client from all channels
       Object.values(channelClients).forEach((clients) => {
         clients.delete(socket);
