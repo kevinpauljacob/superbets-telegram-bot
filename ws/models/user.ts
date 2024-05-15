0a0f5743e4c56e16859d34fd3e22434bcae8aef3
Alwin Helor <alwin24012002@gmail.com>
2024-05-15 20:25:33 +0530

    updating points with ticket buy in exitscam

---
+++ ws/models/user.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    wallet: {
+      type: String,
+      required: true,
+    },
+    solAmount: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    keys: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    stakedAmount: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    tier: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    multiplier: {
+      type: Number,
+      default: 0.5,
+      required: true,
+    },
+    points: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.User || mongoose.model("User", userSchema);
