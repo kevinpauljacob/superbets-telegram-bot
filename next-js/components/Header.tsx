3bed2c9e9cbadf513b2be4d169ecbf254d11423c
Ragul8775 <rb7439@srmist.edu.in>
2024-05-16 22:17:05 +0530

    language dropdown issue( was displaying behind subheader)

--- next-js/components/Header.tsx
+++ next-js/components/Header.tsx
@@ -90,7 +90,7 @@ export function Header({
                 }}
                 className="relative hidden md:flex items-center hover:bg-[#0000009f] transition-all p-1.5 rounded-md"
               >
-                <MdOutlineLanguage className="w-5 h-5 mr-3 text-white text-opacity-50" />
+                <MdOutlineLanguage className="w-5 h-5 mr-3 text-white text-opacity-50 z-[100]" />
                 <span className="text-white text-sm mr-5 text-start text-opacity-50">
                   {language === "en"
                     ? "English"
