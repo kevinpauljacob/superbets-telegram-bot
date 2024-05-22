b4868a7cf8ac85e451648dc7de8f7c5a244e8f10
jxhnsebastian <john.babysebastian@gmail.com>
2024-05-22 10:24:30 +0530

    hydration fix

--- next-js/components/StakeStats.tsx
+++ next-js/components/StakeStats.tsx
@@ -134,13 +134,13 @@ export default function StakeStats() {
             (userData?.stakedAmount ?? 0)}{" "}
           {translator("more $FOMO to reach", language)} T
           {(userData?.tier ?? 0) + 1}
-          <div className="text-[#94A3B8]">
+          <span className="text-[#94A3B8]">
             (
             <span className="text-staking-secondary ml-1">
               {stakingTiers[(userData?.tier ?? 0) + 1]?.multiplier ?? 0.5}x
             </span>{" "}
             {translator("multiplier", language)} )
-          </div>
+          </span>
         </p>
       </div>
       <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mt-2 font-sans">
