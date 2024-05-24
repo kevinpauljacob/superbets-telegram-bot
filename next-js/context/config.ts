ae4bf6e33e6158c87611b305f99a2d1bd83691a5
Ragul8775 <rb7439@srmist.edu.in>
2024-05-24 16:33:58 +0530

    Tokens Added SPL_tokens in Config.ts

--- next-js/context/config.ts
+++ next-js/context/config.ts
@@ -10,4 +10,32 @@ export const SPL_TOKENS = [
     icon: "https://upload.wikimedia.org/wikipedia/en/b/b9/Solana_logo.png",
     betSizes: [1, 3, 5],
   },
+  {
+    tokenName: "BTC",
+    tokenMint: "BTC",
+    decimal: 9,
+    icon: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Bitcoin_Logo.png",
+    betSizes: [1, 3, 5],
+  },
+  {
+    tokenName: "USDC",
+    tokenMint: "USDC",
+    decimal: 9,
+    icon: "https://upload.wikimedia.org/wikipedia/commons/4/49/USDC_Logo.png",
+    betSizes: [1, 3, 5],
+  },
+  {
+    tokenName: "USDT",
+    tokenMint: "USDT",
+    decimal: 9,
+    icon: "https://upload.wikimedia.org/wikipedia/commons/0/01/USDT_Logo.png",
+    betSizes: [1, 3, 5],
+  },
+  {
+    tokenName: "ETH",
+    tokenMint: "ETH",
+    decimal: 9,
+    icon: "https://upload.wikimedia.org/wikipedia/commons/f/fd/Ethereum_Logo.png",
+    betSizes: [1, 3, 5],
+  },
 ];
--- next-js/pages/_app.tsx
+++ next-js/pages/_app.tsx
@@ -16,6 +16,7 @@ import {
 import { GlobalProvider } from "@/components/GlobalContext";
 import "@solana/wallet-adapter-react-ui/styles.css";
 import { SessionProvider } from "next-auth/react";
+import { useEffect } from "react";
 
 export default function App({
   Component,
