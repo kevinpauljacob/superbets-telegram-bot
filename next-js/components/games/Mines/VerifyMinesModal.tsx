2a5f0d2d36c7228b0d248e03f21be1ba05dec933
jxhnsebastian <john.babysebastian@gmail.com>
2024-05-30 00:17:58 +0530

    loader

--- next-js/components/games/Mines/VerifyMinesModal.tsx
+++ next-js/components/games/Mines/VerifyMinesModal.tsx
@@ -8,12 +8,13 @@ import { MdClose } from "react-icons/md";
 import { translator } from "@/context/transactions";
 import Image from "next/image";
 import Bets from "../Bets";
+import Loader from "../Loader";
 
 export interface Mines {
   createdAt: string;
-  minesCount:number;
+  minesCount: number;
   wallet: string;
-  userBets:number[];
+  userBets: number[];
   amount: number;
   result: string;
   risk: string;
@@ -49,12 +50,12 @@ export default function VerifyDice2Modal({
   modalData,
   wallet,
 }: Props) {
- 
   const { bet } = modalData;
   const { getProvablyFairData, language } = useGlobalContext();
 
   //Provably Fair Modal handling
   const [isPFModalOpen, setIsPFModalOpen] = useState(false);
+  const [pfLoading, setPfLoading] = useState(false);
 
   const openPFModal = () => {
     setIsPFModalOpen(true);
@@ -106,14 +107,10 @@ export default function VerifyDice2Modal({
   const modifiedBet = {
     ...bet,
     strikeNumbers: [
-      0, 0, 0, 1, 0, 
-    0, 0, 0, 0, 0, 
-    0, 0, 0, 0, 1, 
-    1, 0, 0, 0, 0, 
-    0, 0, 0, 0, 0 
-  ]  
+      0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ],
   };
-  console.log(modifiedBet)
+  console.log(modifiedBet);
   function formatDate(dateString: string) {
     const date = new Date(dateString);
     const day = date.getUTCDate().toString().padStart(2, "0");
@@ -124,7 +121,7 @@ export default function VerifyDice2Modal({
 
     return `${day}-${month}-${year} ${hours}:${minutes} UTC`;
   }
-console.log("Mines Bet",bet)
+  console.log("Mines Bet", bet);
   return (
     <>
       {isOpen && (
@@ -171,46 +168,49 @@ console.log("Mines Bet",bet)
               </button>
             </div>
             <div className="mt-6 px-4 md:px-12 pt-7 border-2 border-white border-opacity-5 rounded-md">
-            <div className="flex justify-center items-center w-full p-2">
-            <div className="grid grid-cols-5 gap-1 sm:gap-2 text-white text-sm md:text-xl font-chakra">
-      {Array.from({ length: 25 }, (_, index) => index + 1).map(number => (
-        <button 
-          key={number} 
-          className={`${
-            modifiedBet.userBets.includes(number-1) ? 
-              (modifiedBet.strikeNumbers[number - 1] === 0 ? "border-[#FCB10F] bg-[#FCB10F33]" : 
-               modifiedBet.strikeNumbers[number - 1] === 1 ? "border-[#F1323E] bg-[#F1323E33]" : "") 
-              : "bg-[#202329]"
-          } flex items-center justify-center cursor-pointer rounded-md text-center transition-all duration-300 ease-in-out 
+              <div className="flex justify-center items-center w-full p-2">
+                <div className="grid grid-cols-5 gap-1 sm:gap-2 text-white text-sm md:text-xl font-chakra">
+                  {Array.from({ length: 25 }, (_, index) => index + 1).map(
+                    (number) => (
+                      <button
+                        key={number}
+                        className={`${
+                          modifiedBet.userBets.includes(number - 1)
+                            ? modifiedBet.strikeNumbers[number - 1] === 0
+                              ? "border-[#FCB10F] bg-[#FCB10F33]"
+                              : modifiedBet.strikeNumbers[number - 1] === 1
+                              ? "border-[#F1323E] bg-[#F1323E33]"
+                              : ""
+                            : "bg-[#202329]"
+                        } flex items-center justify-center cursor-pointer rounded-md text-center transition-all duration-300 ease-in-out 
           lg2:w-[48px] lg2:h-[48px] md:w-[45px] md:h-[45px] sm:w-[43px] sm:h-[43px] sm2:w-[40px]
           sm2:h-[40px] xs:w-[38px] xs:h-[38px] w-[33px] h-[33px]`}
-        >
-          {modifiedBet.strikeNumbers[number-1] === 0 ? (
-            <div className="w-full h-full flex items-center justify-center p-1.5 sm:p-3">
-              <Image
-                src="/assets/gem.svg"
-                alt="Gem"
-                layout="responsive"
-                height={100}
-                width={100}
-              />
-            </div>
-          ) : modifiedBet.strikeNumbers[number-1] === 1 ? (
-            <div className="w-full h-full flex items-center justify-center p-1.5 sm:p-3">
-              <Image
-                src="/assets/mine.svg"
-                alt="Mine"
-                layout="responsive"
-                height={100}
-                width={100}
-              />
-            </div>
-          ) : null}
-        </button>
-      ))}
-    </div>
-
-            
+                      >
+                        {modifiedBet.strikeNumbers[number - 1] === 0 ? (
+                          <div className="w-full h-full flex items-center justify-center p-1.5 sm:p-3">
+                            <Image
+                              src="/assets/gem.svg"
+                              alt="Gem"
+                              layout="responsive"
+                              height={100}
+                              width={100}
+                            />
+                          </div>
+                        ) : modifiedBet.strikeNumbers[number - 1] === 1 ? (
+                          <div className="w-full h-full flex items-center justify-center p-1.5 sm:p-3">
+                            <Image
+                              src="/assets/mine.svg"
+                              alt="Mine"
+                              layout="responsive"
+                              height={100}
+                              width={100}
+                            />
+                          </div>
+                        ) : null}
+                      </button>
+                    ),
+                  )}
+                </div>
               </div>
               <div className="flex gap-4 pt-7 mb-8">
                 <div className="w-full">
@@ -326,24 +326,34 @@ console.log("Mines Bet",bet)
                         </div>
                         <button
                           className="bg-[#7839C5] rounded-md w-full text-sm text-white text-opacity-90 text-semibold py-3"
+                          disabled={pfLoading}
                           onClick={async () => {
+                            setPfLoading(true);
                             const fpData = await getProvablyFairData();
                             if (fpData)
                               setPFModalData({ ...fpData, tab: "seeds" });
+                            setPfLoading(false);
 
                             openPFModal();
                           }}
                         >
-                          {translator("Rotate", language)}
+                          {pfLoading ? (
+                            <Loader />
+                          ) : (
+                            translator("Rotate", language)
+                          )}
                         </button>
                       </>
                     ) : (
                       <button
                         className="bg-[#7839C5] rounded-md w-full text-sm text-white text-opacity-90 text-semibold py-3"
+                        disabled={pfLoading}
                         onClick={async () => {
+                          setPfLoading(true);
                           const fpData = await getProvablyFairData();
                           if (fpData)
                             setPFModalData({ ...fpData, tab: "verify" });
+                          setPfLoading(false);
 
                           openPFModal();
                         }}
