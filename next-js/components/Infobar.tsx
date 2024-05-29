21f5ee66026058cad13e478851f763fe14c0df05
jxhnsebastian <john.babysebastian@gmail.com>
2024-05-29 14:28:31 +0530

    exit game timers

--- next-js/components/Infobar.tsx
+++ next-js/components/Infobar.tsx
@@ -1,6 +1,11 @@
 import { translator } from "@/context/transactions";
 import { useGlobalContext } from "./GlobalContext";
 import { useState, useEffect } from "react";
+import dynamic from "next/dynamic";
+
+const CountdownTimer = dynamic(() => import("./CountdownTimer"), {
+  ssr: false,
+});
 
 export default function InfoBar() {
   const { language } = useGlobalContext();
@@ -19,7 +24,7 @@ export default function InfoBar() {
   }, []);
 
   return (
-    <div className="w-full min-h-[2rem] mt-1 z-[100] bg-[linear-gradient(90deg,#1D3B7C_0%,#1D3B7C_100%)] text-[#E7E7E7] text-opacity-70 flex items-center justify-center text-xs gap-5">
+    <div className="w-full min-h-[2rem] mt-2 z-[100] bg-[linear-gradient(90deg,#1D3B7C_0%,#1D3B7C_100%)] text-[#E7E7E7] text-opacity-70 flex items-center justify-center text-xs gap-5">
       <div className="flex items-center gap-1">
         <span className="text-[#e7e7e7] text-opacity-70 text-xs font-normal">
           {translator("Unique Players", language)} :
@@ -41,6 +46,15 @@ export default function InfoBar() {
           SOL
         </span>
       </div>
+      <div className="flex items-center gap-1">
+        <span className="text-[#e7e7e7] text-opacity-70 text-xs font-normal">
+          {translator("Exit Game", language)} :
+        </span>
+        <span className="text-[#e7e7e7] text-opacity-70 text-xs font-medium">
+        <CountdownTimer targetDate={new Date(Date.UTC(2024, 5, 2, 19, 30, 0))} />
+        </span>
+      </div>
+      
     </div>
   );
 }
