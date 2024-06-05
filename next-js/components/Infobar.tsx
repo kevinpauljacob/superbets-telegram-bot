6a39868af7778f342595d85969135f3d5001bed8
Shishir435 <shishirchaurasiya435@gmail.com>
2024-06-05 15:25:40 +0530

    Topbar dynamic carousal for mobile view

--- next-js/components/Infobar.tsx
+++ next-js/components/Infobar.tsx
@@ -15,6 +15,25 @@ export default function InfoBar() {
     totalPlayers: 0,
   });
 
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slides = [
+    {
+      label: translator("Unique Players", language),
+      value: stats.totalPlayers,
+    },
+    {
+      label: translator("Total Volume", language),
+      value: `${(stats.totalVolume ?? 0).toLocaleString("en-US", {
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 2,
+      })} SOL`,
+    },
+    {
+      label: translator("Exit Game", language),
+      value: <CountdownTimer targetDate={new Date(Date.UTC(2024, 5, 2, 19, 30, 0))} />,
+    },
+  ];
+
   useEffect(() => {
     fetch("/api/games/global/getAggStats")
       .then((res) => res.json())
@@ -23,38 +42,35 @@ export default function InfoBar() {
       });
   }, []);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, [slides.length]);
+
   return (
     <div className="w-full min-h-[2rem] z-[100] bg-[linear-gradient(90deg,#1D3B7C_0%,#1D3B7C_100%)] text-[#E7E7E7] text-opacity-70 flex items-center justify-center text-xs gap-5">
-      <div className="flex items-center gap-1">
+      {/* samll screens (mobile) */}
+      <div className="w-full flex md:hidden justify-center ">
+          <div className="flex items-center gap-1">
+            <span className="text-[#e7e7e7] text-opacity-70 text-xs font-normal">
+              {slides[currentSlide].label} :
+            </span>
+            <span className="text-[#e7e7e7] text-opacity-70 text-xs font-medium">
+              {slides[currentSlide].value}
+            </span>
+          </div>
+      </div>
+      {/* medium screen */}
+      {slides.map(({label,value})=>(<div className="hidden md:flex items-center gap-1">
         <span className="text-[#e7e7e7] text-opacity-70 text-xs font-normal">
-          {translator("Unique Players", language)} :
+          {label} :
         </span>
         <span className="text-[#e7e7e7] text-opacity-70 text-xs font-medium">
-          {stats.totalPlayers}
+          {value}
         </span>
-      </div>
-      <div className="flex items-center gap-1">
-        <span className="text-[#e7e7e7] text-opacity-70 text-xs font-normal">
-          {translator("Total Volume", language)} :
-        </span>
-        <span className="text-[#e7e7e7] text-opacity-70 text-xs font-medium">
-          {" "}
-          {(stats.totalVolume ?? 0).toLocaleString("en-US", {
-            minimumFractionDigits: 0,
-            maximumFractionDigits: 2,
-          })}{" "}
-          SOL
-        </span>
-      </div>
-      <div className="flex items-center gap-1">
-        <span className="text-[#e7e7e7] text-opacity-70 text-xs font-normal">
-          {translator("Exit Game", language)} :
-        </span>
-        <span className="text-[#e7e7e7] text-opacity-70 text-xs font-medium">
-        <CountdownTimer targetDate={new Date(Date.UTC(2024, 5, 2, 19, 30, 0))} />
-        </span>
-      </div>
-      
+      </div>))}
     </div>
   );
 }
