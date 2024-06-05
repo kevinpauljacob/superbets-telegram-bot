ca3c807cb8cbec611a7f93dae99d260d835cb424
Shishir435 <shishirchaurasiya435@gmail.com>
2024-06-05 14:19:28 +0530

    Hide bets image and reduce margin-top from 4rem to 2rem for smaller screens.

--- next-js/components/table/Table.tsx
+++ next-js/components/table/Table.tsx
@@ -218,7 +218,7 @@ export const TableButtons: React.FC<TableButtonProps> = ({ all, setAll }) => {
         home ? "" : "mt-20"
       }`}
     >
-      <div className="flex items-center">
+      <div className="hidden md:flex items-center ">
         <Image src={Dollar} alt="" width={26} height={26} />
         <span className="font-semibold font-changa text-xl text-white text-opacity-90 pl-3">
           {translator("Bets", language)}
