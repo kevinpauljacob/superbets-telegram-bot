0bf1cbe2d94a557f980f1c07b647f2bc94ec4325
itaintkevin <kpjtofficial@gmail.com>
2024-06-17 08:05:18 +0530

    moved modal to layout

--- next-js/components/affiliate-program/CreateCampaignModal.tsx
+++ next-js/components/affiliate-program/CreateCampaignModal.tsx
@@ -4,26 +4,23 @@ import { translator } from "@/context/transactions";
 import { useGlobalContext } from "@/components/GlobalContext";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { successCustom, errorCustom } from "../toasts/ToastGroup";
+import { AdaptiveModal, AdaptiveModalContent } from "../AdaptiveModal";
 
-type Props = {
-  modal: boolean;
-  setModal: (modal: boolean) => void;
-};
-
-export default function CoinFlipProvablyFairModal({ modal, setModal }: Props) {
+export default function CreateCampaignModal() {
   const wallet = useWallet();
-  const { language } = useGlobalContext();
+  const { language, showCreateCampaignModal, setShowCreateCampaignModal } =
+    useGlobalContext();
   const [campaignName, setCampaignName] = useState("");
   const [campaignCode, setCampaignCode] = useState("");
 
-  const handleClose = () => {
-    //@ts-ignore
-    document.addEventListener("click", function (event) {
-      //@ts-ignore
-      var targetId = event.target.id;
-      if (targetId && targetId === "pf-modal-bg") setModal(false);
-    });
-  };
+  // const handleClose = () => {
+  //   //@ts-ignore
+  //   document.addEventListener("click", function (event) {
+  //     //@ts-ignore
+  //     var targetId = event.target.id;
+  //     if (targetId && targetId === "pf-modal-bg") setModal(false);
+  //   });
+  // };
 
   const createCampaign = async (event: any) => {
     event.preventDefault();
@@ -44,7 +41,7 @@ export default function CoinFlipProvablyFairModal({ modal, setModal }: Props) {
 
       if (success) {
         successCustom(message);
-        setModal(false);
+        setShowCreateCampaignModal(false);
       } else {
         errorCustom("Failed to create campaign!");
       }
@@ -57,61 +54,51 @@ export default function CoinFlipProvablyFairModal({ modal, setModal }: Props) {
   };
 
   return (
-    <div
-      onClick={() => {
-        handleClose();
-      }}
-      id="pf-modal-bg"
-      className="absolute z-[150] left-0 top-0 flex h-full w-full items-center justify-center bg-[#33314680] backdrop-blur-[0px] transition-all"
+    <AdaptiveModal
+      open={showCreateCampaignModal}
+      onOpenChange={() => setShowCreateCampaignModal(false)}
     >
-      <div className="bg-[#121418] max-h-[80dvh]  overflow-y-scroll p-8 rounded-lg z-10 w-11/12 sm:w-[32rem] -mt-[4.7rem] md:mt-0 nobar">
-        <div className="flex font-chakra tracking-wider text-2xl font-semibold text-[#F0F0F0] items-center justify-between">
-          <div className="flex items-center gap-2 font-changa">
-            {translator("Create Campaign", language)}
+      <AdaptiveModalContent className="bg-[#121418] sm:overflow-y-auto min-h-[50dvh] max-h-[80dvh] w-full pb-6">
+        <div className="px-8 sm:p-0">
+          <div className="flex font-chakra tracking-wider text-2xl font-semibold text-[#F0F0F0] items-center justify-between">
+            <div className="flex items-center gap-2 font-changa">
+              {translator("Create Campaign", language)}
+            </div>
           </div>
-          <div className="hover:cursor-pointer">
-            <MdClose
-              size={25}
-              color="#F0F0F0"
-              onClick={() => {
-                setModal(false);
-              }}
-            />
-          </div>
+          <form onSubmit={(event) => createCampaign(event)}>
+            <div className="mt-4">
+              <label className="text-xs font-changa text-opacity-90 text-[#F0F0F0]">
+                {translator("Campaign Name", language)}
+              </label>
+              <input
+                type="text"
+                name="Campaign Name"
+                value={campaignName}
+                onChange={(e) => setCampaignName(e.target.value)}
+                className="font-chakra bg-[#202329] text-[#94A3B8] text-base mt-1 rounded-[5px] px-5 py-4 w-full relative flex items-center justify-between"
+              />
+            </div>
+            <div className="mt-4">
+              <label className="text-xs font-changa text-opacity-90 text-[#F0F0F0]">
+                {translator("Code (Campaign ID)", language)}
+              </label>
+              <input
+                type="text"
+                name="Code (Campaign ID)"
+                value={campaignCode}
+                onChange={(e) => setCampaignCode(e.target.value)}
+                className="font-chakra bg-[#202329] text-[#94A3B8] text-base mt-1 rounded-[5px] px-5 py-4 w-full relative flex items-center justify-between"
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-[#7839C5] rounded-[5px] font-chakra uppercase font-semibold text-lg text-center text-white w-full mt-11 py-4"
+            >
+              {translator("Create Campaign", language)}
+            </button>
+          </form>
         </div>
-        <form onSubmit={(event) => createCampaign(event)}>
-          <div className="mt-4">
-            <label className="text-xs font-changa text-opacity-90 text-[#F0F0F0]">
-              {translator("Campaign Name", language)}
-            </label>
-            <input
-              type="text"
-              name="Campaign Name"
-              value={campaignName}
-              onChange={(e) => setCampaignName(e.target.value)}
-              className="font-chakra bg-[#202329] text-[#94A3B8] text-base mt-1 rounded-[5px] px-5 py-4 w-full relative flex items-center justify-between"
-            />
-          </div>
-          <div className="mt-4">
-            <label className="text-xs font-changa text-opacity-90 text-[#F0F0F0]">
-              {translator("Code (Campaign ID)", language)}
-            </label>
-            <input
-              type="text"
-              name="Code (Campaign ID)"
-              value={campaignCode}
-              onChange={(e) => setCampaignCode(e.target.value)}
-              className="font-chakra bg-[#202329] text-[#94A3B8] text-base mt-1 rounded-[5px] px-5 py-4 w-full relative flex items-center justify-between"
-            />
-          </div>
-          <button
-            type="submit"
-            className="bg-[#7839C5] rounded-[5px] font-chakra uppercase font-semibold text-lg text-center text-white w-full mt-11 py-4"
-          >
-            {translator("Create Campaign", language)}
-          </button>
-        </form>
-      </div>
-    </div>
+      </AdaptiveModalContent>
+    </AdaptiveModal>
   );
 }
