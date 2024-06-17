7346ba9b8d7aeb9ffa86b4e0ef972480b1d214bd
itaintkevin <kpjtofficial@gmail.com>
2024-06-17 08:04:51 +0530

    copy function

--- next-js/components/affiliate-program/ReferralLink.tsx
+++ next-js/components/affiliate-program/ReferralLink.tsx
@@ -1,5 +1,6 @@
 import { formatNumber } from "@/context/transactions";
 import { useState, useEffect } from "react";
+import { copyToClipboard } from "@/pages/affiliate-program";
 
 export default function ReferralLink({
   campaignName,
@@ -11,6 +12,8 @@ export default function ReferralLink({
   totalEarnings: Record<string, any>;
 }) {
   const [earnings, setEarnings] = useState(0);
+  const [buttonText, setButtonText] = useState("Copy");
+
   const calculatedTotalEarnings = () => {
     let total = 0;
     for (let key in totalEarnings) {
@@ -24,7 +27,7 @@ export default function ReferralLink({
   }, [totalEarnings]);
 
   return (
-    <div className="flex items-center justify-between bg-staking-bg rounded-[10px] py-4 px-5 mb-4">
+    <div className="flex items-center justify-between bg-staking-bg rounded-[10px] py-4 px-3.5 sm:px-5 mb-4">
       <div className="flex flex-col sm:flex-row sm:items-center gap-[14px]">
         <p className="text-white font-chakra font-semibold sm:text-lg">
           {campaignName}
@@ -33,8 +36,19 @@ export default function ReferralLink({
           <span className="bg-white/5 rounded-[5px] text-sm font-chakra text-[#94A3B8] font-normal px-4 py-2">
             {`referralCode=${referralCode}`}
           </span>
-          <button className="bg-[#7839C5] rounded-[5px] text-white/75 text-[13px] font-chakra font-medium px-5">
-            Copy
+          <button
+            onClick={() => {
+              copyToClipboard(
+                `http://localhost:3000?referralCode=${referralCode}`,
+              );
+              setButtonText("Copied!");
+              setTimeout(() => {
+                setButtonText("Copy");
+              }, 2000);
+            }}
+            className="bg-[#7839C5] hover:bg-[#9361d1] focus:bg-[#602E9E] transition-all cursor-pointer rounded-[5px] text-white/75 text-[13px] font-chakra font-medium px-5"
+          >
+            {buttonText}
           </button>
         </div>
       </div>
